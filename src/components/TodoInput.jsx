121319bd6b59ed21732e0dad753315cd4a722c71
Luji <zlfjrii@gmail.com>
2023-12-26 12:11:32 +0700

    Modifying TodoInput: added function for handling add new todo

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,15 +1,44 @@
+import { useState } from 'react';
 import logo from '../assets/logo.png';
 
 export default function TodoInput() {
+  const [allTodos, setAllTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleAddNewTodo = () => {
+    if (newTodo === '') {
+      console.log('woy, masih kosong!');
+    } else {
+      let newTodoObj = {
+        todo: newTodo,
+        completed: false,
+      };
+      let updatedTodoArr = [...allTodos];
+      updatedTodoArr.push(newTodoObj);
+      setAllTodos(updatedTodoArr);
+      localStorage.setItem('todolist', JSON.stringify(updatedTodoArr));
+      localStorage.clear;
+      setNewTodo('');
+    }
+  };
+
   return (
     <>
       <div className="lg:flex lg:mb-8">
         <div className="flex items-center justify-center">
-          <img src={logo} alt="logo" className="w-1/3 h-auto lg:w-[50%] md:w-[30%]" />
+          <img src={logo} alt="logo" className="w-1/3 h-auto lg:w-[50%] md:w-[25%]" />
         </div>
         <div className="flex items-center justify-center m-10 lg:w-[60%] lg:m-5">
-          <input type="text" name="input" className="w-svw bg-[#303030] p-2 rounded-sm mr-3 shadow-inner outline-none" placeholder="Mau ngapain hari ini?" />
-          <button type="button" className="bg-[#D4C321] text-black p-2 rounded-sm shadow-inner hover:bg-[#9B8E14] transition">
+          <input
+            type="text"
+            value={newTodo}
+            onChange={(e) => {
+              setNewTodo(e.target.value);
+            }}
+            placeholder="Mau ngapain hari ini?"
+            className="w-svw bg-[#303030] p-2 rounded-sm mr-3 shadow-inner outline-none"
+          />
+          <button type="button" onClick={handleAddNewTodo} className="bg-[#D4C321] text-black p-2 rounded-sm shadow-inner hover:bg-[#9B8E14] transition">
             Submit
           </button>
         </div>
